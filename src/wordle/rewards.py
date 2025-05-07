fec6abe3c101f66ca3d80a148d8d1f24e7350891
symegac <97731141+symegac@users.noreply.github.com>
2025-05-07 13:27:54 +0200

    Create reward system

---
+++ src/wordle/rewards.py
@@ -0,0 +1,123 @@
+from tabulate import tabulate
+import pandas as pd
+
+WORD_LENGTH = 5
+ALLOWED_GUESSES = 6
+
+class Rewards:
+    def __init__(
+        self,
+        word_length: int = 5,
+        allowed_guesses: int = 6
+    ):
+        self.word_length = word_length
+        self.allowed_guesses = allowed_guesses
+
+    def get(
+        self,
+        guesses: int | None = None,
+        green: int | None = None,
+        yellow: int | None = None,
+    ) -> int | None:
+        if guesses is None:
+            guesses = self.allowed_guesses
+        if green is None and yellow is not None:
+            green = 0
+        elif yellow is None:
+            green = self.word_length
+        if yellow is None:
+            yellow = 0
+
+        if not all(isinstance(param, int) for param in (guesses, green, yellow)):
+            raise TypeError("Alle inddata skal være heltal.")
+
+        if guesses < 0 or guesses > self.allowed_guesses:
+            raise ValueError(f"Antal gæt kan kun være heltal fra 1 til {self.allowed_guesses}.")
+
+        # Hvis alle bogstaver er grønne og antal gæt er inden for det tilladte,
+        # så beregnes belønning
+        if green == self.word_length:
+            return self._calculate_reward(guesses)
+
+        # Straf kan kun uddeles, hvis det maksimale antal gæt er brugt
+        if guesses < self.allowed_guesses:
+            raise ValueError(f"Man kan ikke måle strafpoint for et spil, der endnu ikke er slut. Antal gæt ({guesses}) er under maksimum ({self.allowed_guesses}), men wordlen er ikke løst.")
+
+        # Mængden af grønne og gule bogstaver skal passe til ordlængden
+        if any((
+            green + yellow > self.word_length,
+            yellow > self.word_length,
+            green > self.word_length
+        )):
+            raise ValueError(f"Den angivne mængde af gule ({yellow}) og grønne ({green}) bogstaver må hverken hver især eller tilsammen overstige ordlængden ({self.word_length}).")
+        if green < 0 or yellow < 0:
+            raise ValueError("De angivne mængder for gule og grønne bogstaver skal være positive heltal.")
+
+        return self._calculate_punishment(green, yellow)
+
+    def _calculate_reward(
+        self,
+        guesses: int,
+    ) -> int:
+        return 2 * self.word_length + 2**(self.allowed_guesses - guesses) - self.allowed_guesses
+
+    def _calculate_punishment(
+        self,
+        green: int,
+        yellow: int,
+    ) -> int:
+        return 2 * green - (self.word_length - (green + yellow)) - 2 * self.word_length
+
+    def generate_reward_table(
+        self,
+        format: bool = False,
+        **kwargs
+    ) -> dict[int, int] | str:
+        table = {}
+        for guesses in range(1, self.allowed_guesses + 1):
+            reward = self._calculate_reward(guesses)
+            table.setdefault(guesses, reward)
+
+        if format:
+            table = tabulate((table.keys(), table.values()), **kwargs)
+        return table
+
+    def generate_punishment_table(
+        self,
+        format: bool = False,
+        **kwargs
+    ) -> list[list[int | None]] | str:
+        table = []
+        for green in range(self.word_length):
+            row = []
+            for yellow in range(self.word_length + 1):
+                if (green + yellow) > self.word_length:
+                    row.append(None)
+                    continue
+                punishment = self._calculate_punishment(green, yellow)
+                row.append(punishment)
+            table.append(row)
+
+        if format:
+            table = tabulate(table, **kwargs)
+        return table
+
+if __name__ == "__main__":
+    rewards = Rewards(5, 6)
+    markdown_fmt = {
+        "headers":    "keys",
+        "showindex":  True,
+        "stralign":   "right",
+        "missingval": '—',
+        "tablefmt":   "pipe"
+    }
+
+    rtable = rewards.generate_reward_table(format=True, tablefmt="pipe", headers="firstrow")
+    ptable = rewards.generate_punishment_table(format=True, **markdown_fmt)
+    print(rtable, ptable, sep='\n')
+    print(rewards.get(3, 5, 0)) # 3 gæt, alt korrekt
+    print(rewards.get(6, 2, 1)) # 6 gæt, 2 grønne, 1 gul
+    print(rewards.get())        # 6 gæt, alt korrekt
+    print(rewards.get(2))       # 2 gæt, alt korrekt
+    print(rewards.get(6, 4))    # 6 gæt, 4 grønne
+    print(rewards.get(yellow=4))# 6 gæt, 0 grønne, 4 gule
